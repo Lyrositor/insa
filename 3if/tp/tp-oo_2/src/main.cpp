1cc7c54d6ae5cfaa0165ecbaa6d81a1c968ff40c
selmanemmaoui <snemmaoui@hotmail.com>
2015-10-20 17:40:06 +0200

    Create project for TP OO 2

---
+++ 3if/tp/tp-oo_2/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main (){
+   // City city = new City();
+    string command;
+    while (true) {
+        cin >> command;
+    }
+
+
+    return 0;
+}
+
