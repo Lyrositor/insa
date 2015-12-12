ce7968f17f69dff55088c8ec54d77ec996722ff0
Lyrositor <gagne.marc@gmail.com>
2015-12-12 11:14:39 +0100

    Allow copying of ConfigReader, since it does not store file streams

--- 3if/oo/tp-oo_3/src/ConfigReader.h
+++ 3if/oo/tp-oo_3/src/ConfigReader.h
@@ -51,22 +51,11 @@ public:
     // Mode d'emploi : Essaie de trouver la clé <key>. Si elle existe, renvoie
     // la valeur associée ; sinon, renvoie la valeur par défaut <def>.
 
-//------------------------------------------------------- Surcharge d'opérateurs
-    ConfigReader & operator = (const ConfigReader & reader) = delete;
-    // <reader> : le lecteur à copier
-    // Mode d'emploi : Supprimé. La copie est interdite pour éviter d'avoir des
-    // conflits liés aux flux.
-
 //-------------------------------------------------- Constructeurs - destructeur
     ConfigReader (const std::string & filename);
     // <filename> : le nom du fichier de configuration à lire
     // Mode d'emploi : Initialise le lecteur à partir du fichier de
     // configuration spécifié par <filename>.
-
-    ConfigReader (const ConfigReader & reader) = delete;
-    // <reader> : le lecteur à copier
-    // Mode d'emploi : Supprimé. La copie est interdite pour éviter d'avoir des
-    // conflits liés aux flux.
 
     virtual ~ConfigReader ();
     // Mode d'emploi : Détruit le lecteur.
