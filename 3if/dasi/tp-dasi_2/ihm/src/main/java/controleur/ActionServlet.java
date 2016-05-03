ba594b06789be5b0b202117df996facdb192ccbf
ArnaudCode <jeux.arnaud@orange.fr>
2016-05-03 17:59:38 +0200

    Try to access DB with JPA (doesn't work)

--- 3if/dasi/tp-dasi_2/ihm/src/main/java/controleur/ActionServlet.java
+++ 3if/dasi/tp-dasi_2/ihm/src/main/java/controleur/ActionServlet.java
@@ -1,12 +1,18 @@
 package controleur;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import metier.modele.Activite;
+import metier.service.ServiceMetier;
 
 @WebServlet(name = "ActionServlet", urlPatterns = {"/ActionServlet"})
 public class ActionServlet extends HttpServlet {
@@ -23,12 +29,38 @@ public class ActionServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        if (request.getParameter("action").equals("listeActivite")) {
-            response.setContentType("application/json;charset=UTF-8");
-            try (PrintWriter out = response.getWriter()) {
-                out.println("{\"activites\":[\"badminton\", \"hockey\"]}");
-            }
+        if (request.getParameter("action") == null) {
+            response.sendRedirect(getServletContext().getContextPath());
+            return;
         }
+
+        Gson gson = new Gson();
+
+        switch (request.getParameter("action")) {
+            case "listeActivite":
+                response.setContentType("application/json;charset=UTF-8");
+                try (PrintWriter out = response.getWriter()) {
+                    String json = gson.toJson(listerActivites());
+
+                    out.println(json);
+                }
+                break;
+            default:
+                response.sendRedirect(getServletContext().getContextPath());
+                break;
+        }
+    }
+
+    private static String listerActivites() {
+        List<Activite> activites = null;
+        try {
+            activites = ServiceMetier.listerActivites();
+        } catch (Throwable ex) {
+            Logger.getLogger(ActionServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Actvivite :");
+        System.out.println(activites);
+        return activites.toString();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
